8553ed00bfa759f6dd90994aa3d7416d49561fee
rahulram21 <rahulkannom21@gmail.com>
2024-04-04 07:19:11 +0530

    latest changes_minor

--- Back end/Index.js
+++ Back end/Index.js
@@ -3,6 +3,7 @@ const app = express();
 http = require('http');
 const cors = require('cors');
 const { Server } = require('socket.io');
+const CHAT_BOT = 'chatbot'
 
 app.use(cors()); //cors middleware
 
@@ -21,5 +22,19 @@ const io = new Server(server, {
 
 
 io.on("connection", (socket) => {
-    console.log(`User connected ${socket.id}`)
+    console.log(`User connected ${socket.id}`);
+
+    socket.on('join_room', (data) => {
+        const {username, room} = data; //data sent from client to server using socket.emit('join_room',data)
+        socket.join(room); //join the client to the specified {room}
+    });
+
+    let __createdTime__ = Date.now(); // timestamp
+
+    //emit the message to client socket
+    socket.to(room).emit('received_message', {
+        message: `${username} has joined chat`,
+        username: CHAT_BOT,
+        __createdTime__
+    })
 })
